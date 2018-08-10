8dfc2d9e7c01dc3ffc62a3cea756b7ae3a2b7148
Zack Mariscal <zmariscal@gmail.com>
2018-08-09 20:42:21 -0700

    uniqueness and downcase validation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -47,4 +47,11 @@ class UserTest < ActiveSupport::TestCase
     assert_not duplicate_user.valid?
   end
 
+  test "email addresses should be saved as downcase" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
 end
